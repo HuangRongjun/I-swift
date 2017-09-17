9ebfbd72e52361dceeef2744edbf5e68a3e420d9
黄荣君 <l1091462907@gmail.com>
2017-09-17 23:45:17 +0800

    整数和浮点数的范例

--- i-swift.playground/Contents.swift
+++ i-swift.playground/Contents.swift
@@ -7,3 +7,36 @@ var str = "Hello, playground"
 var a = 2;
 var b = 3;
 var result = a * b;
+
+var sum = 0;
+for i in 0...100{
+    sum += i;
+}
+print(sum);
+
+let PI = 3.1455926;
+var pi = 3.14e-2
+
+Int.max
+Int.min
+
+Int64.max
+Int64.min
+
+let fiftenInDecimal = 15
+let fiftenInHex = 0xF
+let fiftenInOctal = 0o17
+let fiftenInBinary = 0b1111
+
+// 千分位
+let million = 1_000_000
+
+var oneThirdFload: Float = 1/3
+var oneThridDouble: Double = 1/3
+
+print(oneThirdFload)
+print(oneThridDouble)
+
+type(of: PI)
+type(of: oneThirdFload)
+type(of: oneThridDouble)
