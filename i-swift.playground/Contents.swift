81c257bf084b0d7740f398c705ee3a7a30af9a65
黄荣君 <l1091462907@gmail.com>
2017-09-24 21:48:28 +0800

    Array 的基本使用范例

--- i-swift.playground/Contents.swift
+++ i-swift.playground/Contents.swift
@@ -5,7 +5,7 @@ import UIKit
 var str = "Hello, playground"
 
 var a = 5
-var b = 6
+var b = 3
 let sum = a + b
 let sub = a - b 
 let mul = a * b
@@ -44,3 +44,66 @@ print(oneThridDouble)
 type(of: PI)
 type(of: oneThirdFload)
 type(of: oneThridDouble)
+
+// Create an empty array
+var array1: Array<Int> = Array<Int>()
+var array2: [Int] = []
+var array3 = array2
+
+// Initialization with values
+var threeInts = [Int](repeating:3, count: 3)
+var sixInts = threeInts + threeInts
+var fiveInts = [1, 2, 3, 4, 5]
+
+// Common arributes
+fiveInts.count
+array1.isEmpty
+
+// Access element
+fiveInts[2]
+// fiveInts[5]
+fiveInts[0...2]
+type(of: fiveInts[0..<2])
+
+// Traverse
+for value in fiveInts{
+    print(value)
+}
+
+for (index, value) in fiveInts.enumerated(){
+    print(index,value)
+}
+
+fiveInts.forEach(){
+    print($0)
+}
+
+// Array modification
+array1.append(1)
+array1 += [2, 3, 4]
+
+array1.insert(0, at: array1.startIndex)
+array1.insert(5, at: array1.endIndex)
+
+array1.remove(at: 4)
+array1.removeLast()
+
+// array2.removeLast()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
