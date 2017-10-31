69910c7d34de14be49dc3dd1c129cd7899887b36
黄荣君 <l1091462907@gmail.com>
2017-11-01 00:30:21 +0800

    add Dictionary

---
+++ i-playround.playground/Pages/chap10_Dictionary.xcplaygroundpage/Contents.swift
@@ -0,0 +1,14 @@
+//: [Previous](@previous)
+
+import Foundation
+
+var moiveRatings = ["Donnie Darko": 4,
+                    "Chungking Express": 5,
+                    "Dark City": 4]
+let darkoRating = moiveRatings["Donnie Darko"]
+
+for (key, value) in moiveRatings {
+    print("THe movie \(key) was rated \(value)")
+}
+
+//: [Next](@next)
