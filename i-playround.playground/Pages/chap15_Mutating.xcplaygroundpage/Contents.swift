118778e5c35a7d71794d255d441e58a83c307766
黄荣君 <l1091462907@gmail.com>
2017-11-12 23:00:01 +0800

    explain how mutating running

--- i-playround.playground/Pages/chap15_Mutating.xcplaygroundpage/Contents.swift
+++ i-playround.playground/Pages/chap15_Mutating.xcplaygroundpage/Contents.swift
@@ -9,15 +9,41 @@ func greet(name: String, withGreeting greeting: String) -> String {
 let personnalGreeting = greet(name: "Matt", withGreeting: "Hello.")
 print(personnalGreeting)
 
-func greeting(forName name: String) -> (String) -> String {
-    func greeting(_ greeting: String) -> String {
-        return "\(greeting) \(name)"
+//func greeting(forName name: String) -> (String) -> String {
+//    func greeting(_ greeting: String) -> String {
+//        return "\(greeting) \(name)"
+//    }
+//    return greeting
+//}
+//
+//let greetMattWith = greeting(forName: "Matt")
+//let mattGreeting = greetMattWith("Hello.")
+//print(mattGreeting)
+
+func greeting(_ greeting: String) -> (String) -> String {
+    return {
+        (name: String) -> String in
+            return "\(greeting) \(name)"
     }
-    return greeting
 }
 
-let greetMattWith = greeting(forName: "Matt")
-let mattGreeting = greetMattWith("Hello.")
-print(mattGreeting)
+let friendlyGreetingFor = greeting("Hello.")
+let mattGreeting = friendlyGreetingFor("Matt")
 
+struct Person {
+    var firstName = "Matt"
+    var lastName = "Mathias"
+    
+    mutating func chageTo (firstName: String, lastName: String) {
+        self.firstName = firstName
+        self.lastName = lastName
+    }
+}
+
+var person = Person()
+let changeName = Person.chageTo
+let changeNameFromMattTo = changeName(&person)
+changeNameFromMattTo("John", "Gallagher")
+person.firstName
+person.chageTo(firstName: "Eva", lastName: "Green")
 //: [Next](@next)
